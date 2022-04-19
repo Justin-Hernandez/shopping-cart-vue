6d5b347152a0ec01640686d92b75c04c830f3e4e
Justin-Hernandez <38103361+Justin-Hernandez@users.noreply.github.com>
2022-04-20 00:50:59 +0200

    zxc

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div className="container-lg align-items-center">
+    <div className="row g-2 vh-100">
+      <div className="col-8 col-md-9">
+        <div className="d-flex justify-content-center">
+          <h1></h1>
+        </div>
+
+        <!-- Categorias -->
+      </div>
+
+      <div className="col-4 col-md-3">
+        <div className="d-flex justify-content-center">
+          <h1></h1>
+        </div>
+
+        <!-- Carrito --> 
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.col-8 {
+  background-color: aquamarine;
 }
+
+.col-4 {
+  background-color: darkorange
+}
+
 </style>
